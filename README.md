5825eaf18d806e1e5b48f7e78c51f324c68b6372
jarju <jarjudrag@gmail.com>
2023-06-11 20:48:55 +0530

    modifications in code and added readme

---
+++ README.md
@@ -0,0 +1,9 @@
+# Recursive funcrtions
+
+## recursive factorial
+
+ -to build a factorial function using recursion
+
+## recursive fibonacci
+
+ -to build a fibonacci function using recursion and Memoisation
--- recursion.c
+++ recursion.c
@@ -1,12 +1,15 @@
 #include <stdio.h>
+#include <math.h>
+#include <windows.h>
+#include <stdlib.h>
+#define MAX 100
 
+  int fac(int n);
+  int fib(int n);
+
+  int ex_cache[MAX];
    
-    int fib(int n){
-        if(n==1)
-          return 1;
-        
-        return n +  fib(n-1);
-    }
+
 
 int main(){
 
@@ -16,6 +19,34 @@ int main(){
    printf("Enter the Nth number : ");
    scanf("%d", &num);
 
+   for (int i = 0; i < MAX; i++)
+   {
+      ex_cache[i] = -1;
+   }
+   
+
+  // printf("\n   Nth  factorial number :  %d\n", fac(num));
+
    printf("\n Sum of Nth fibonacci number : %d\n", fib(num));
     
+}
+
+ int fib(int n){
+        if(n<=1)
+          return n;
+
+        if(ex_cache[n] != -1){
+          return ex_cache[n];
+        }
+
+        ex_cache[n] = fib(n-1) + fib(n-2);
+        return ex_cache[n];
+ }
+
+
+int fac(int n){
+        if(n==1)
+          return 1;
+        
+        return n* fac(n-1);
 }
